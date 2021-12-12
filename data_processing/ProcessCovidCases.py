5127476a76a4cb333b786030b440ed683d67ac75
Danny Lu <danny.lu075@gmail.com>
2021-12-11 19:52:57 -0500

    Update ProcessCovidCases.py

--- data_processing/ProcessCovidCases.py
+++ data_processing/ProcessCovidCases.py
@@ -19,19 +19,26 @@ class ProcessCovidCases:
             new_deaths = data[4]
             cumulative_deaths = data[5]
 
+            # Convert the date reported value from a string into a datetime object.
             date = datetime.strptime(date_reported, 'Y-%m')
+
+            # Get the day value for creating daily case instances.
             day = datetime.strptime(date_reported, '%d')
 
-            # 
+            # If the date is not in the dictionary, intialize it with a list of a daily case object.
             if date not in date_to_cases:
                 date_to_cases[date] = [DailyCases(country_code, cumulative_cases, new_cases, day)]
             
+            # If the date is already a key, add to it's list value with a daily case object.
             else:
                 date_to_cases[date].append(DailyCases(country_code, cumulative_cases, new_cases, day))
 
         
+        # Iterate through every key in the date_to_cases dictionary
         for month in date_to_cases:
             month_list_cases = date_to_cases[month]
             cumulative_cases = date_to_cases[month][-1]
+            
+            # create a monthly covid case object for each iteration and then append it to the _monthly_covid_cases list attribute.
             monthly_covid_case = MonthlyCovidCases(month_list_cases, cumulative_cases, month.month)
             self._monthly_covid_cases.append(monthly_covid_case)
