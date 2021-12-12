eb01f56e899ec4bbefd3546c2401b7c3ce4997e0
Danny Lu <danny.lu075@gmail.com>
2021-12-11 19:49:33 -0500

    Update ProcessCovidCases.py

--- data_processing/ProcessCovidCases.py
+++ data_processing/ProcessCovidCases.py
@@ -1,12 +1,39 @@
-import csv
-from typing import Any
-from data_loading.DataLoad import DataLoad
+from datetime import datetime
+from entities.MonthlyCases import MonthlyCovidCases
+from entities.DailyCases import DailyCases
 
 
-class LoadShipping(DataLoad):
+class ProcessCovidCases:
+    def __init__(self) -> None:
+        self._monthly_covid_cases = []
 
-    def __init__(self, raw_data: Any):
-        super().__init__(raw_data)
+    def process_monthly_cases(self, raw_covid_data: list[(str, str, int, int, int, int)]) -> None:
+        cumulative_cases = 0
 
-    def load_data(self):
-        """Loads csv data into raw_data"""
+        # Maps year-month to list of daily cases
+        date_to_cases = {}
+
+        for data in raw_covid_data:
+            date_reported = data[0]
+            country_code = data[1]
+            new_cases = data[2]
+            cumulative_cases = data[3]
+            new_deaths = data[4]
+            cumulative_deaths = data[5]
+
+            date = datetime.strptime(date_reported, 'Y-%m')
+            day = datetime.strptime(date_reported, '%d')
+
+            # 
+            if date not in date_to_cases:
+                date_to_cases[date] = [DailyCases(country_code, cumulative_cases, new_cases, day)]
+            
+            else:
+                date_to_cases[date].append(DailyCases(country_code, cumulative_cases, new_cases, day))
+
+        
+        for month in date_to_cases:
+            month_list_cases = date_to_cases[month]
+            cumulative_cases = date_to_cases[month][-1]
+            monthly_covid_case = MonthlyCovidCases(month_list_cases, cumulative_cases, month.month)
+            self._monthly_covid_cases.append(monthly_covid_case)
