4d3671f50720c18ff318b4b498481a1879222fe0
Clark Zhang <68950923+czhang0312@users.noreply.github.com>
2021-12-12 23:06:41 -0500

    Update ProcessUnemployment.py

--- data_processing/ProcessUnemployment.py
+++ data_processing/ProcessUnemployment.py
@@ -5,7 +5,6 @@ from Utilities.Utils import QUARTERS
 
 class ProcessUnemployment:
     """Process the raw data from LoadUnemployment"""
-    _monthly_ship_trades: []
 
     def __init__(self) -> None:
         self._monthly_unemployment = []
@@ -20,20 +19,21 @@ class ProcessUnemployment:
 
         # Populate the dictionary mapping
         for data in raw_unemployment_data:
-            date = data[0]
-            quarter = QUARTERS[data.date.month]
-            year = data.date.year
+            date_list = data[0].split('-')  # date is list of strings
+            year = int(date_list[0])
+            month = int(date_list[1])
+            quarter = QUARTERS[month]
             year_quarter = str(year) + '~' + str(quarter)
             unemployment_rate = data[1]
 
             if year_quarter not in date_to_list_monthly:
-                date_to_list_monthly[year_quarter] = [MonthlyUnemployment(date, unemployment_rate)]
+                date_to_list_monthly[year_quarter] = [MonthlyUnemployment(month, year, unemployment_rate)]
             else:
-                date_to_list_monthly[year_quarter].append(MonthlyUnemployment(date, unemployment_rate))
+                date_to_list_monthly[year_quarter].append(MonthlyUnemployment(month, year, unemployment_rate))
 
         # Create list of QuarterlyUnemployment objects
-        for key in date_to_list_monthly:
-            list_monthly = date_to_list_monthly[key]
-            quarter = QUARTERS[date_to_list_monthly[key][0].date.month]
-            year = date_to_list_monthly[key][0].date.year
-            self._quarterly_unemployment.append(QuarterlyUnemployment(list_monthly, quarter, year))
+        for yq in date_to_list_monthly:
+            list_monthly = date_to_list_monthly[yq]
+            quarter = QUARTERS[list_monthly[0].month]
+            year = list_monthly[0].year
+            self._quarterly_unemployment.append(QuarterlyUnemployment(list_monthly, year, quarter))
