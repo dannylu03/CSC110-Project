24023bdbd28004fa5073aefb8f4ef85feb9606e3
Danny Lu <danny.lu075@gmail.com>
2021-12-12 20:28:19 -0500

    Update QuarterlyCovidCases.py

--- entities/QuarterlyCovidCases.py
+++ entities/QuarterlyCovidCases.py
@@ -1,40 +1,37 @@
-"""Insert Module shit here
-"""
+from entities.MonthlyCases import MonthlyCovidCases
 from datetime import datetime
-from DailyCases import DailyCases
 
-
-class MonthlyCovidCases:
-    """Contains methods to calculate stats for covid cases in a month
-    """
-
-    def __init__(self, monthly_cases: list[DailyCases], cumulative_cases: int, month: datetime, year: datetime) -> None:
-        """Initialize a new ????
-        """
-        self._monthly_cases = monthly_cases
+class QuarterlyCovidCases:
+    def __init__(self, quarter_cases: list[MonthlyCovidCases], cumulative_cases: int, quarter: int, year: datetime) -> None:
+        self._quarter_cases = quarter_cases
         self._cumulative_cases = cumulative_cases
-        self._month = month
+        self._quarter = quarter
         self._year = year
-    
+
     def calculate_average_daily_increase(self) -> float:
-        """Return a float value of the average daily increase of this month instance. 
+        """Return a float value of the average daily increase of this quarter instance. 
         """
 
         # Initialize accumulator which will be returned later
         average_daily_increase = 0.0
 
         # Initialize num_days as the length of this month object's list of daily covid cases
-        num_days = len(self._monthly_cases)
+        num_days = len(self._month_list_cases)
 
-        total_new_cases = sum([d.new_cases for d in self._monthly_cases])
+        total_new_cases = 0
+
+        for month in self._quarter_cases:
+            for day in month:
+                total_new_cases += day.new_cases
         
         average_daily_increase = total_new_cases / num_days
         return average_daily_increase
 
-    def calculate_total_monthly_increase(self) -> float:
+    def calculate_total_quarterly_increase(self) -> float:
         """Return the total increase in cumulative cases this month"""
-        last_day = len(self._monthly_cases) - 1
-        last_day_cases = self._monthly_cases[last_day].cumulative_cases
-        first_day_cases = self._monthly_cases[0].cumulative_cases
+        first_day_cases = self._quarter_cases[0]._month_list_cases[0].cumulative_cases
+
+        last_day_cases = self._quarter_cases[-1]._month_list_cases[-1].cumulative_cases
+
 
         return last_day_cases - first_day_cases
