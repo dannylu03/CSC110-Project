65a23ccc707de1bdba4ed71a6a641c07ab079d9a
Clark Zhang <68950923+czhang0312@users.noreply.github.com>
2021-12-13 17:38:07 -0500

    Update QuarterlyGdp.py

--- entities/QuarterlyGdp.py
+++ entities/QuarterlyGdp.py
@@ -1,9 +1,16 @@
+"""CSC110 Final Project: Entities Quarterly GDP
+
+Copyright and Usage Information
+===============================
+
+This file is Copyright (c) 2021 Clark Zhang, Danny Lu, Alex Balaria, Yue Fung Lee.
+"""
 from dataclasses import dataclass
 
 
 @dataclass
 class QuarterlyGdp:
-
+    """GDP growth of US in a quarter"""
     quarter: int
     year: int
     growth: float
