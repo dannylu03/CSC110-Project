bab1b6bbeb6d40603052b020563022789c793d68
Danny Lu <danny.lu075@gmail.com>
2021-12-12 23:26:10 -0500

    Update ProcessCovidCases.py

--- data_processing/ProcessCovidCases.py
+++ data_processing/ProcessCovidCases.py
@@ -5,8 +5,8 @@ from entities.DailyCases import DailyCases
 from Utilities.Utils import QUARTERS
 
 class ProcessCovidCases:
-    _monthly_covid_cases: list[DailyCases]
-    _quarterly_covid_cases: list[MonthlyCovidCases]
+    _monthly_covid_cases: list[MonthlyCovidCases]
+    _quarterly_covid_cases: list[QuarterlyCovidCases]
 
     def __init__(self) -> None:
         self._monthly_covid_cases = []
@@ -39,7 +39,7 @@ class ProcessCovidCases:
             year = int(date_list[0])
             month = int(date_list[1])
 
-            # Convert datetime object into a string used as keys for the dates_and_daily_cases dictionary. 
+            # Convert integer into a string used as keys for the dates_and_daily_cases dictionary. 
             year_month = str(year) + '-' + str(month)
 
             # If the date is not in the dates_and_daily_cases dictionary, intialize it with a list of a daily case object.
@@ -59,7 +59,7 @@ class ProcessCovidCases:
             # In this case, the last day of the month represents the last Daily Case object in the dictionary list value.
             cumulative_cases = dates_and_daily_cases[date][-1].cumulative_cases
 
-            # Since all daily cases belong in the same month and year for each key, taking the date.month and date.year attributes of any daily case object will work.
+            # Since all daily cases belong in the same month and year for each key, taking the month and year attributes of any daily case object will work.
             month = dates_and_daily_cases[date][0].month
             year = dates_and_daily_cases[date][0].year
 
@@ -94,13 +94,13 @@ class ProcessCovidCases:
             else:
                 dates_and_monthly_cases[year_quarter].append(monthly_case)
         
-        for quarter in dates_and_monthly_cases:
-            quarterly_cases = dates_and_monthly_cases[quarter]
-            cumulative_cases = dates_and_monthly_cases[quarter][-1].cumulative_cases
+        for key in dates_and_monthly_cases:
+            quarterly_cases = dates_and_monthly_cases[key]
+            cumulative_cases = dates_and_monthly_cases[key][-1].cumulative_cases
 
-            month = dates_and_monthly_cases[quarter][0].month
+            month = dates_and_monthly_cases[key][0].month
             quarter = QUARTERS[int(month)]
 
-            quarterly_covid_cases = QuarterlyCovidCases(quarterly_cases, cumulative_cases, quarter, dates_and_monthly_cases[quarter][0].year)
+            quarterly_covid_cases = QuarterlyCovidCases(quarterly_cases, cumulative_cases, quarter, dates_and_monthly_cases[key][0].year)
 
             self._quarterly_covid_cases.append(quarterly_covid_cases)
