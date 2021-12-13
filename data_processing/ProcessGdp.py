7df54629be02c9f5b0a347d5b6f470e0c2f8aabd
Clark Zhang <68950923+czhang0312@users.noreply.github.com>
2021-12-13 17:30:51 -0500

    Update ProcessGdp.py

--- data_processing/ProcessGdp.py
+++ data_processing/ProcessGdp.py
@@ -1,23 +1,27 @@
+"""CSC110 Final Project: Data Processing Process Gdp
+
+Copyright and Usage Information
+===============================
+
+This file is Copyright (c) 2021 Clark Zhang, Danny Lu, Alex Balaria, Yue Fung Lee.
+"""
 from entities.QuarterlyGdp import QuarterlyGdp
 
+
 class ProcessGdp:
+    """Processes the raw gdp data from LoadGdp"""
 
     quarterly_gdp: [QuarterlyGdp]
 
-    def __init__(self):
+    def __init__(self) -> None:
         self.quarterly_gdp = []
 
-    def process_gdp(self, raw_gdp_data: list[(str, float)]) -> list[QuarterlyGdp]:
-        """
-            Processes quarterly gdp data
-        """
+    def process_gdp(self, raw_gdp_data: list[(str, float)]) -> None:
+        """Processes quarterly gdp data"""
         # discard any previous data
         self.quarterly_gdp = []
         for data in raw_gdp_data:
-            # processes quarter and year as it is stored as a string (example: Q1 '11)
-            split_data = data[0].split(' ')
+            split_data = data[0].split(' ')  # data[0] is string representation of date
             quarter = int(split_data[0][1])
-            # dataset stores last 2 digits of years, since dataset only has data from 2000s and to be consistent
-            # with other datasets 2000 is added
             year = 2000 + int(split_data[1].split('\'')[1])
             self.quarterly_gdp.append(QuarterlyGdp(quarter, year, data[1]))
