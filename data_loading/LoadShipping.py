c7570be9c140c59a5718eaaf5a3aef416f4cb201
Clark Zhang <68950923+czhang0312@users.noreply.github.com>
2021-12-13 17:28:19 -0500

    Update LoadShipping.py

--- data_loading/LoadShipping.py
+++ data_loading/LoadShipping.py
@@ -1,20 +1,26 @@
+"""CSC110 Final Project: Data Loading: Load Shipping
+
+Copyright and Usage Information
+===============================
+
+This file is Copyright (c) 2021 Clark Zhang, Danny Lu, Alex Balaria, Yue Fung Lee.
+"""
 from data_loading.DataLoad import DataLoad
 import csv
 
 
 class LoadShipping(DataLoad):
+    """Load Shipping data"""
 
     def __init__(self, file_path: str):
         """Initializes LoadShipping data class"""
         super().__init__(file_path)
+        print(file_path)
         self.raw_data = []
 
     def load_data(self) -> list[(int, int, int)]:
-        """
-        Loads data from provided file path
+        """ Loads data from provided file path
 
-        Preconditions:
-            - self.file_path != ''
 
         """
         with open(self.file_path) as raw_shipping_file:
@@ -24,6 +30,7 @@ class LoadShipping(DataLoad):
             next(raw_file_data)
             for row in raw_file_data:
                 # Appends raw_data with relevant entries from the data set
-                self.raw_data.append((int(row[5]), int(row[-2]), int(row[-1])))
+                if int(row[-1]) > 2010:
+                    self.raw_data.append((int(row[5]), int(row[-2]), int(row[-1])))
         return self.raw_data
 
