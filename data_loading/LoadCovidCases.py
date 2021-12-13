4bc415cb6abcc536607c44fb3221db79e62ca42d
Clark Zhang <68950923+czhang0312@users.noreply.github.com>
2021-12-13 17:27:53 -0500

    Update LoadCovidCases.py

--- data_loading/LoadCovidCases.py
+++ data_loading/LoadCovidCases.py
@@ -1,15 +1,24 @@
+"""CSC110 Final Project: Data Loading: Load Covid Cases
+
+Copyright and Usage Information
+===============================
+
+This file is Copyright (c) 2021 Clark Zhang, Danny Lu, Alex Balaria, Yue Fung Lee.
+"""
 from data_loading.DataLoad import DataLoad
 import csv
 
 
 class LoadCovidCases(DataLoad):
-
+    """Load covid data"""
     def __init__(self, file_path: str) -> None:
         super().__init__(file_path)
         self.raw_data = []
 
     def load_data(self) -> list[(str, str, int, int)]:
         """ Loads data from provided file path
+
+
          """
         with open(self.file_path, 'r') as raw_covid_data:
             raw_file_data = csv.reader(raw_covid_data)
