375ed975583d7b3adbc33b7edb114ee348585a55
Clark Zhang <68950923+czhang0312@users.noreply.github.com>
2021-12-13 17:39:23 -0500

    Update ShipTrade.py

--- entities/ShipTrade.py
+++ entities/ShipTrade.py
@@ -1,8 +1,16 @@
+"""CSC110 Final Project: Entities ShipTrade
+
+Copyright and Usage Information
+===============================
+
+This file is Copyright (c) 2021 Clark Zhang, Danny Lu, Alex Balaria, Yue Fung Lee.
+"""
 import datetime
 from dataclasses import dataclass
 
 
 @dataclass
 class ShipTrade:
+    """A shipment across a US border"""
     date: datetime
     value: int
