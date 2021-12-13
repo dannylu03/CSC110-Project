505478a6cf2927ab58fdb1e5dcecfdd2f0113a65
Clark Zhang <68950923+czhang0312@users.noreply.github.com>
2021-12-13 15:04:44 -0500

    Update MonthlyUnemployment.py

--- entities/MonthlyUnemployment.py
+++ entities/MonthlyUnemployment.py
@@ -1,4 +1,10 @@
-"""Insert module shit here"""
+"""CSC110 Final Project: Entities Monthly Unemployment
+
+Copyright and Usage Information
+===============================
+
+This file is Copyright (c) 2021 Clark Zhang, Danny Lu, Alex Balaria, Yue Fung Lee.
+"""
 from dataclasses import dataclass
 
 
